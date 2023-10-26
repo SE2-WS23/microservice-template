871ac50868cd0243c937164c0f146503c8736420
Chris <chrissy.drx@gmail.com>
2023-10-26 18:19:50 +0200

    Add: Example Code and ToDos

--- main.py
+++ main.py
@@ -4,23 +4,33 @@ import functions_framework
 
 
 @functions_framework.http
-def hello_http(request):
+def http_function(request):
     """HTTP Cloud Function.
     Args:
         request (flask.Request): The request object.
         <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
     Returns:
-        The response text, or any set of values that can be turned into a
-        Response object using `make_response`
+        A set of values that can be turned into a Response object using
+        ‚`make_response`
         <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
     """
-    request_json = request.get_json(silent=True)
-    request_args = request.args
 
-    if request_json and "name" in request_json:
-        name = request_json["name"]
-    elif request_args and "name" in request_args:
-        name = request_args["name"]
-    else:
-        name = "World"
-    return f"Hello {escape(name)}!"
+    # ToDo: Add your functionality
+
+    # ToDo: Set header, status_code and response. You response can have any value
+    # that can be turned into a Repsonse object using `make_reponse'.
+    # See more https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response
+    status_code = 200
+    header = {}
+    response = {}
+
+    return response, status_code, header
+
+
+# For testing purposes only!
+# It is easier to develop your function without the need to restart your cloud function every time
+# But keep in mind that this is not completely the same environment as in the cloud function environment
+if __name__ == "__main__":
+    # ToDo: Add your function here
+
+    pass
