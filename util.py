455a5f2efb61146c47adbc2a58e8a8d5bb6924db
Chris <chrissy.drx@gmail.com>
2023-12-08 13:15:35 +0100

    Add: More Example Code

--- util.py
+++ util.py
@@ -1,22 +1,24 @@
 def handle_preflight(
-    allowed_methods="POST, GET, PUT, PATCH, DELETE, OPTIONS",
-    allowed_headers="Content-Type",
+    allowed_methods=["POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowed_headers=["Content-Type"],
     max_age="3600",
 ):
-    """Handles CORS preflight requests.
-    Args:
-        allowed_methods (str): Comma separated list of allowed HTTP methods.
-        allowed_headers (str): Comma separated list of allowed HTTP headers.
-        max_age (str): Maximum number of seconds the client/browser should cache
-            the preflight response.
-    Returns:
-        A set of values that can be turned into a Response object using
-        `make_response`
-        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response
+    """
+    The `handle_preflight` function sets the necessary headers for handling preflight requests in a web
+    application.
+
+    :param allowed_methods: A list of HTTP methods that are allowed for the preflight request. The
+    default value includes common methods such as POST, GET, PUT, PATCH, DELETE, and OPTIONS
+    :param allowed_headers: The `allowed_headers` parameter is a list of headers that are allowed in the
+    preflight request. In this case, only the "Content-Type" header is allowed
+    :param max_age: The `max_age` parameter specifies the maximum time in seconds that the preflight
+    response can be cached by the client. In this case, it is set to "3600", which means the preflight
+    response can be cached for 1 hour, defaults to 3600 (optional)
+    :return: an empty string, a status code of 204 (No Content), and a dictionary of headers.
     """
     headers = {
-        "Access-Control-Allow-Methods": allowed_methods,
-        "Access-Control-Allow-Headers": allowed_headers,
+        "Access-Control-Allow-Methods": ", ".join(allowed_methods),
+        "Access-Control-Allow-Headers": ", ".join(allowed_headers),
         "Access-Control-Max-Age": max_age,
     }
 
